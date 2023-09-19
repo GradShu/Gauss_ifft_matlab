a1222616c4f5286cac2d512554ae09a2c0dc74ee
GradShu <137853002+GradShu@users.noreply.github.com>
2023-09-19 18:34:28 +0800

    Add files via upload

--- signal_processing.m
+++ signal_processing.m
@@ -10,81 +10,99 @@ clear;close all
 %        c1 =      0.3129
 %Creat a phase functoion:
 %      pSigma = -4*pi*z0*sigma;
-nPoint = 8192;
+
+%Related parameters
+%intensityMark,phaseMark,ifftIntenMark,ifftPhaseMark:image identification
+intensityMark = 221;
+phaseMark = 223;
+ifftIntenMark = 222;
+ifftPhaseMark = 224;
+%npoint:sampling point 
+nPoint = 2^16;
 z0 = 10;
-sigma = 0:0.0083:8191*0.0083;
+sigma = 0:0.0083:(nPoint-1)*0.0083;
 fSigma = 568.8*exp(-((sigma-1.45)/0.3129).^2);
 pSigma = -4*pi*z0*sigma;
 iSigma = fSigma.*exp(1i*pSigma);
 %calculate ifft of iSigma
-[intensity,phase,zData]=ifftOfIsigma(iSigma,sigma(1),sigma(end),nPoint);
+[intensity,phase,zDataInter]=ifftOfIsigma(iSigma,sigma(1),sigma(end),nPoint);
 
 figure(1);
 subplot(2,2,1);
-setIntensityImage(sigma,fSigma,'Wavenumber \sigma(\mum^{-1})','Intensity(a.u.)');
+setImage(221,sigma,fSigma,'Wavenumber \sigma(\mum^{-1})','Intensity(a.u.)');
 subplot(2,2,3);
-setPhaseImage(sigma,pSigma,'Wavenumber \sigma(\mum^{-1})','Phase(rad)');
+setImage(223,sigma,pSigma,'Wavenumber \sigma(\mum^{-1})','Phase(rad)');
 subplot(2,2,2);
-setIntensityImage(zData,intensity,'Position z(\mum)','Intensity(a.u.)');
+setImage(222,zDataInter,intensity,'Position z(\mum)','Intensity(a.u.)');
 subplot(2,2,4);
-setPhaseImage(zData,phase,'Position z(\mum)','Phase(rad)');
+setImage(224,zDataInter,phase,'Position z(\mum)','Phase(rad)');
 
-function setIntensityImage(xData,yData,xlabel,ylabel)
+function setImage(imageMark,xData,yData,xlabel,ylabel)
 %This function setImage use for setting image property, such as axes, line,
 %colour and so on. 
+%ampMaxZ:The abscissa where the maximum amplitude value is located.
+global ampMaxZ
 yDataLine = plot(xData,yData);
 %Find the max of Intensity.
 [yDataMax,yDataMaxindex]= max(yData);
-hold on
-yMaxLine = plot([xData(yDataMaxindex),xData(yDataMaxindex)],[0,yDataMax]);
 %Set the location of the tick marks along the axis
+if imageMark == 221
+    hold on
+    ampMaxZ = xData(yDataMaxindex);
+    yMaxLine = plot([xData(yDataMaxindex),xData(yDataMaxindex)],...
+               [0,yDataMax]);
+    set(gca,'Xlim',   [xData(yDataMaxindex)-0.7 xData(yDataMaxindex)+0.7]);
+    set(yDataLine,'color',      'blue',...
+                  'LineStyle',  '-',...
+                  'LineWidth',   2);
+    set(yMaxLine, 'color',       'k',...
+                  'LineStyle',  '--',...
+                  'LineWidth',   2);
+elseif imageMark == 223
+    set(gca,'Xlim',   [ampMaxZ-0.7 ampMaxZ+0.7]);
+    set(yDataLine,'color',      'blue',...
+                  'LineStyle',  '-',...
+                  'LineWidth',   2);
+elseif imageMark == 222
+    hold on
+    ampMaxZ = xData(yDataMaxindex);
+    yMaxLine = plot([xData(yDataMaxindex),xData(yDataMaxindex)],...
+               [0,yDataMax]);
+    set(yDataLine,'color',      'blue',...
+                  'LineStyle',  '-',...
+                  'LineWidth',   2);
+    set(yMaxLine, 'color',       'k',...
+                  'LineStyle',   '--',...
+                  'LineWidth',    2);
+    set(gca,'Xlim',   [xData(yDataMaxindex)-2 xData(yDataMaxindex)+2]);
+else
+    hold on
+    zeroLine = plot([ampMaxZ-2,ampMaxZ+2],[0,0]);
+    set(gca,'Xlim', [ampMaxZ-2 ampMaxZ+2]);
+    set(yDataLine,'color',      'blue',...
+                  'LineStyle',  '-',...
+                  'LineWidth',   2);
+    set(zeroLine, 'color',       'k',...
+                  'LineStyle',   '--',...
+                  'LineWidth',    2);
+end
+
 set(gca,'Xcolor',     [0 0 0],...
         'Ycolor',     [0 0 0],...
         'Color' ,     [1 1 1],...
         'FontName',   'Arial',...
         'FontAngle',  'normal',...
-        'FontSize',    13,...
-        'Xlim',       [xData(yDataMaxindex)-0.7 xData(yDataMaxindex)+0.7]);
+        'FontSize',    13);
 %Set label names for X coordinates and Y coordinates
-set(get(gca,'XLabel'),      'String',xlabel,...
-                            'FontName','Arial',...
-                            'FontSize',14);
-set(get(gca,'YLabel'),      'String',ylabel,...
-                            'FontName','Arial',...
-                            'FontSize',14);
-set(yDataLine,'color',      'blue',...
-              'LineStyle',  '-',...
-              'LineWidth',   2);
-set(yMaxLine,'color',       'k',...
-              'LineStyle',  '-.',...
-              'LineWidth',   2);
+set(get(gca,'XLabel'), 'String',xlabel,...
+                       'FontName','Arial',...
+                       'FontSize',14);
+set(get(gca,'YLabel'), 'String',ylabel,...
+                       'FontName','Arial',...
+                       'FontSize',14);
 end
 
-function setPhaseImage(xData,yData,xlabel,ylabel)
-%This function setImage use for setting image property, such as axes, line,
-%colour and so on. 
-yDataLine = plot(xData,yData);
-% zeroPhase = ;
-%Set the location of the tick marks along the axis
-set(gca,'Xcolor',     [0 0 0],...
-        'Ycolor',     [0 0 0],...
-        'Color' ,     [1 1 1],...
-        'FontName',   'Arial',...
-        'FontAngle',  'normal',...
-        'FontSize',   13);
-%Set label names for X coordinates and Y coordinates
-set(get(gca,'XLabel'),'String',xlabel,...
-                      'FontName','Arial',...
-                      'FontSize',14);
-set(get(gca,'YLabel'),'String',ylabel,...
-                      'FontName','Arial',...
-                      'FontSize',14);
-set(yDataLine,'color',      'blue',...
-              'LineStyle',  '-',...
-              'LineWidth',   2);
-end
-
-function [intensity,phase,zData]=ifftOfIsigma(input,sigmaStart,sigmaEnd,nPoint)
+function [intensity,phase,zDataInter]=ifftOfIsigma(input,sigmaStart,sigmaEnd,nPoint)
 %This function use for calculating ifft of iSigma.
 %The output is intensity and phase after ifft.
 deltaSigma = (sigmaEnd-sigmaStart)/(nPoint-1);
@@ -92,5 +110,12 @@ deltaZ = 1/(2*nPoint*deltaSigma);
 zData = (1:nPoint)*deltaZ;
 intensity = abs(ifft(input));
 phase = angle(ifft(input));
+%interpolation
+zDataInter = linspace(zData(1),zData(end),nPoint*10);
+intensity = interpn(zData,intensity,zDataInter,'linear');
+phase = interpn(zData,phase,zDataInter,'linear');
 end
 
+function findZeroPhase()
+
+end
